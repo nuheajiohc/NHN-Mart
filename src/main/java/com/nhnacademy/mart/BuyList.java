856a197282968bcf9fa5dfa4adac2877bc270a25
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-09-04 03:33:31 +0900

    feat : BuyList 구현 완료

--- src/main/java/com/nhnacademy/mart/BuyList.java
+++ src/main/java/com/nhnacademy/mart/BuyList.java
@@ -6,13 +6,37 @@ public class BuyList {
 
     private final ArrayList<Item> items = new ArrayList<>();
 
-    // TODO add 메서드 생성
 
+    // TODO add 메서드 생성
+    public void add(String foodInput) {
+        String[] splitedFoodInput = foodInput.split(" ");
+        for (int i = 0; i < splitedFoodInput.length; i += 2) {
+            String name = splitedFoodInput[i];
+            int amount = Integer.parseInt(splitedFoodInput[i + 1]);
+            items.add(new Item(name, amount));
+        }
+
+    }
+
+    public ArrayList<Item> getItemsList() {
+        return items;
+    }
 
     public static class Item {
         private final String name;
         private final int amount;
 
+        public Item(String name, int amount) {
+            this.name = name;
+            this.amount = amount;
+        }
 
+        public String getName() {
+            return name;
+        }
+
+        public int getAmount() {
+            return amount;
+        }
     }
 }
