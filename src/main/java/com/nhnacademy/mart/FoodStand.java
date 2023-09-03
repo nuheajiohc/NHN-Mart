6d4625634ea2b447249bed9f520285da01072dc7
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-09-04 03:33:16 +0900

    feat : foodStand 구현 완료

--- src/main/java/com/nhnacademy/mart/FoodStand.java
+++ src/main/java/com/nhnacademy/mart/FoodStand.java
@@ -7,6 +7,20 @@ public class FoodStand {
     private final ArrayList<Food> foods = new ArrayList<>();
 
     // TODO add 메서드 구현
+    public void add(Food food){
+        foods.add(food);
+    }
 
     // TODO 장바구니에 담은 Food 삭제 구현
+    public boolean remove(Food food){
+        return foods.remove(food);
+    }
+
+    public Food checkValidFood(String name){
+        for(Food food : foods){
+            if(food.getName().equals(name))
+                return food;
+        }
+        return null;
+    }
 }
