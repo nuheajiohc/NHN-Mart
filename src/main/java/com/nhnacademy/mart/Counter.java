7479b36c68e6fcb2b0025c2f5454e34f7d04d9a9
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2023-09-04 03:34:03 +0900

    feat : Counter 클래스 구현 완료

--- src/main/java/com/nhnacademy/mart/Counter.java
+++ src/main/java/com/nhnacademy/mart/Counter.java
@@ -1,7 +1,26 @@
 package com.nhnacademy.mart;
 
+import java.util.Iterator;
+
 public class Counter {
+    private Basket basket;
+    private int totalPrice = 0;
 
     // TODO pay 메서드 구현 (카운터에서 계산 처리 메서드)
 
+    public void getCustomerBasket(Basket basket) {
+        this.basket = basket;
+    }
+
+    public void pay() {
+        Iterator<Food> iter = basket.getFoodsList().iterator();
+        while (iter.hasNext()) {
+            int price = iter.next().getPrice();
+            this.totalPrice += price;
+        }
+    }
+
+    public int getTotalPrice() {
+        return this.totalPrice;
+    }
 }
